361beaf7de815570fc9290e36b9a448dfa8eed6e
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-10-05 19:53:24 +0800

    update: fix overflowing sign up success

--- lib/presentation/screens/auth/signup/signup_success_screen.dart
+++ lib/presentation/screens/auth/signup/signup_success_screen.dart
@@ -12,10 +12,9 @@ class SignupSuccessScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(24),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
                 padding: const EdgeInsets.all(24),
