7c415c9f41c526ef13fe44fc430b9266f4c7b34a
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-10-06 07:43:35 +0800

    update: custom text field

--- lib/presentation/widgets/custom_text_field.dart
+++ lib/presentation/widgets/custom_text_field.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController? controller;
-  final String label;
+  final String? label;
+  final String? labelText; // Alias for label
   final String? hint;
+  final String? hintText; // Alias for hint
   final String? errorText;
   final bool obscureText;
   final TextInputType keyboardType;
@@ -14,12 +17,17 @@ class CustomTextField extends StatelessWidget {
   final FormFieldValidator<String>? validator;
   final bool enabled;
   final TextInputAction? textInputAction;
+  final List<TextInputFormatter>? inputFormatters;
+  final String? suffixText;
+  final TextCapitalization textCapitalization;
 
   const CustomTextField({
     super.key,
     this.controller,
-    required this.label,
+    this.label,
+    this.labelText,
     this.hint,
+    this.hintText,
     this.errorText,
     this.obscureText = false,
     this.keyboardType = TextInputType.text,
@@ -30,20 +38,28 @@ class CustomTextField extends StatelessWidget {
     this.validator,
     this.enabled = true,
     this.textInputAction,
+    this.inputFormatters,
+    this.suffixText,
+    this.textCapitalization = TextCapitalization.none,
   });
 
   @override
   Widget build(BuildContext context) {
+    final effectiveLabel = labelText ?? label;
+    final effectiveHint = hintText ?? hint;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          label,
-          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                fontWeight: FontWeight.w600,
-              ),
-        ),
-        const SizedBox(height: 8),
+        if (effectiveLabel != null) ...[
+          Text(
+            effectiveLabel,
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+          const SizedBox(height: 8),
+        ],
         TextFormField(
           controller: controller,
           obscureText: obscureText,
@@ -53,11 +69,14 @@ class CustomTextField extends StatelessWidget {
           validator: validator,
           enabled: enabled,
           textInputAction: textInputAction,
+          inputFormatters: inputFormatters,
+          textCapitalization: textCapitalization,
           decoration: InputDecoration(
-            hintText: hint,
+            hintText: effectiveHint,
             errorText: errorText,
             prefixIcon: prefixIcon,
             suffixIcon: suffixIcon,
+            suffixText: suffixText,
             contentPadding: const EdgeInsets.symmetric(
               horizontal: 16,
               vertical: 16,
