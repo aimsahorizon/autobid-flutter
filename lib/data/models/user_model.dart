6c9b360be1667932ede6d4b5b248a938ab59bd3f
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-10-07 22:30:03 +0800

    update: add kyc fields for enhanced kyc

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,116 +1,88 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'kyc_model.dart';
 
-class UserModel {
-  final String id;
-  final String email;
-  final String fullName;
-  final String? firstName;
-  final String? middleName;
-  final String? lastName;
-  final DateTime? dateOfBirth;
-  final String? gender;
-  final String? phoneNumber;
-  final String? street;
-  final String? barangay;
-  final String? city;
-  final String? province;
-  final String? zipCode;
-  final String? nationality;
-  final bool termsAccepted;
-  final bool privacyAccepted;
-  final String kycStatus; // 'pending', 'verified', 'rejected'
-  final String accountType; // 'individual', 'business'
-  final DateTime createdAt;
-  final bool verifiedBadge;
-  final KycModel? kycData;
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
 
-  const UserModel({
-    required this.id,
-    required this.email,
-    required this.fullName,
-    this.firstName,
-    this.middleName,
-    this.lastName,
-    this.dateOfBirth,
-    this.gender,
-    this.phoneNumber,
-    this.street,
-    this.barangay,
-    this.city,
-    this.province,
-    this.zipCode,
-    this.nationality,
-    this.termsAccepted = false,
-    this.privacyAccepted = false,
-    this.kycStatus = 'pending',
-    this.accountType = 'individual',
-    required this.createdAt,
-    this.verifiedBadge = false,
-    this.kycData,
-  });
+enum VerificationLevel {
+  @JsonValue('unverified')
+  unverified,
+  @JsonValue('level1')
+  level1,
+  @JsonValue('level2')
+  level2,
+  @JsonValue('level3')
+  level3,
+  @JsonValue('dealer')
+  dealer,
+}
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'email': email,
-      'fullName': fullName,
-      'firstName': firstName,
-      'middleName': middleName,
-      'lastName': lastName,
-      'dateOfBirth': dateOfBirth?.toIso8601String(),
-      'gender': gender,
-      'phoneNumber': phoneNumber,
-      'street': street,
-      'barangay': barangay,
-      'city': city,
-      'province': province,
-      'zipCode': zipCode,
-      'nationality': nationality,
-      'termsAccepted': termsAccepted,
-      'privacyAccepted': privacyAccepted,
-      'kycStatus': kycStatus,
-      'accountType': accountType,
-      'createdAt': createdAt.toIso8601String(),
-      'verifiedBadge': verifiedBadge,
-      'kycData': kycData?.toJson(),
-    };
-  }
+enum ProofOfAddressType {
+  @JsonValue('utility_bill')
+  utilityBill,
+  @JsonValue('bank_statement')
+  bankStatement,
+  @JsonValue('government_letter')
+  governmentLetter,
+  @JsonValue('rental_contract')
+  rentalContract,
+  @JsonValue('barangay_certificate')
+  barangayCertificate,
+}
 
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      id: json['id'] as String,
-      email: json['email'] as String,
-      fullName: json['fullName'] as String,
-      firstName: json['firstName'] as String?,
-      middleName: json['middleName'] as String?,
-      lastName: json['lastName'] as String?,
-      dateOfBirth: json['dateOfBirth'] != null
-          ? DateTime.parse(json['dateOfBirth'] as String)
-          : null,
-      gender: json['gender'] as String?,
-      phoneNumber: json['phoneNumber'] as String?,
-      street: json['street'] as String?,
-      barangay: json['barangay'] as String?,
-      city: json['city'] as String?,
-      province: json['province'] as String?,
-      zipCode: json['zipCode'] as String?,
-      nationality: json['nationality'] as String?,
-      termsAccepted: json['termsAccepted'] as bool? ?? false,
-      privacyAccepted: json['privacyAccepted'] as bool? ?? false,
-      kycStatus: json['kycStatus'] as String? ?? 'pending',
-      accountType: json['accountType'] as String? ?? 'individual',
-      createdAt: DateTime.parse(json['createdAt'] as String),
-      verifiedBadge: json['verifiedBadge'] as bool? ?? false,
-      kycData: json['kycData'] != null
-          ? KycModel.fromJson(json['kycData'] as Map<String, dynamic>)
-          : null,
-    );
-  }
+enum BackgroundCheckStatus {
+  @JsonValue('none')
+  none,
+  @JsonValue('pending')
+  pending,
+  @JsonValue('cleared')
+  cleared,
+  @JsonValue('rejected')
+  rejected,
+}
 
-  UserModel copyWith({
-    String? id,
-    String? email,
-    String? fullName,
+@freezed
+abstract class ProofOfAddress with _$ProofOfAddress {
+  const factory ProofOfAddress({
+    required ProofOfAddressType type,
+    required String imageUrl,
+    required DateTime uploadedAt,
+    @Default(false) bool verified,
+  }) = _ProofOfAddress;
+
+  factory ProofOfAddress.fromJson(Map<String, dynamic> json) =>
+      _$ProofOfAddressFromJson(json);
+}
+
+@freezed
+abstract class NbiClearance with _$NbiClearance {
+  const factory NbiClearance({
+    required String imageUrl,
+    required DateTime issueDate,
+    required DateTime expiryDate,
+  }) = _NbiClearance;
+
+  factory NbiClearance.fromJson(Map<String, dynamic> json) =>
+      _$NbiClearanceFromJson(json);
+}
+
+@freezed
+abstract class VerificationLimits with _$VerificationLimits {
+  const factory VerificationLimits({
+    required int buyLimit,
+    required int sellLimit,
+  }) = _VerificationLimits;
+
+  factory VerificationLimits.fromJson(Map<String, dynamic> json) =>
+      _$VerificationLimitsFromJson(json);
+}
+
+@freezed
+abstract class UserModel with _$UserModel {
+  const factory UserModel({
+    required String id,
+    required String email,
+    required String fullName,
     String? firstName,
     String? middleName,
     String? lastName,
@@ -123,37 +95,21 @@ class UserModel {
     String? province,
     String? zipCode,
     String? nationality,
-    bool? termsAccepted,
-    bool? privacyAccepted,
-    String? kycStatus,
-    String? accountType,
-    DateTime? createdAt,
-    bool? verifiedBadge,
+    @Default(false) bool termsAccepted,
+    @Default(false) bool privacyAccepted,
+    @Default('pending') String kycStatus,
+    @Default('individual') String accountType,
+    required DateTime createdAt,
+    @Default(false) bool verifiedBadge,
     KycModel? kycData,
-  }) {
-    return UserModel(
-      id: id ?? this.id,
-      email: email ?? this.email,
-      fullName: fullName ?? this.fullName,
-      firstName: firstName ?? this.firstName,
-      middleName: middleName ?? this.middleName,
-      lastName: lastName ?? this.lastName,
-      dateOfBirth: dateOfBirth ?? this.dateOfBirth,
-      gender: gender ?? this.gender,
-      phoneNumber: phoneNumber ?? this.phoneNumber,
-      street: street ?? this.street,
-      barangay: barangay ?? this.barangay,
-      city: city ?? this.city,
-      province: province ?? this.province,
-      zipCode: zipCode ?? this.zipCode,
-      nationality: nationality ?? this.nationality,
-      termsAccepted: termsAccepted ?? this.termsAccepted,
-      privacyAccepted: privacyAccepted ?? this.privacyAccepted,
-      kycStatus: kycStatus ?? this.kycStatus,
-      accountType: accountType ?? this.accountType,
-      createdAt: createdAt ?? this.createdAt,
-      verifiedBadge: verifiedBadge ?? this.verifiedBadge,
-      kycData: kycData ?? this.kycData,
-    );
-  }
+    @Default(VerificationLevel.unverified) VerificationLevel verificationLevel,
+    VerificationLimits? verificationLimits,
+    ProofOfAddress? proofOfAddress,
+    String? tin,
+    NbiClearance? nbiClearance,
+    @Default(BackgroundCheckStatus.none) BackgroundCheckStatus backgroundCheckStatus,
+  }) = _UserModel;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 }
