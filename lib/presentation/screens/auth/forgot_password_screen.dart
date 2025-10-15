020b7e722b9a9f4076311bf0e3cfeaa0d55bc3b5
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-10-15 10:42:13 +0800

    update: remove data service import

--- lib/presentation/screens/auth/forgot_password_screen.dart
+++ lib/presentation/screens/auth/forgot_password_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../core/constants/color_constants.dart';
-import '../../../data/services/mock/mock_auth_service.dart';
 import '../../providers/auth_provider.dart';
 import '../../widgets/custom_text_field.dart';
 import '../../widgets/custom_button.dart';
