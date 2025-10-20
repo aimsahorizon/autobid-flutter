cfdb23f671e4f207121142c60bbc622e3b4951c7
nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2025-10-20 13:34:49 +0800

    update: add onboarding for constants

--- lib/core/constants/string_constants.dart
+++ lib/core/constants/string_constants.dart
@@ -5,6 +5,7 @@ class StringConstants {
 
   // Routes
   static const String splashRoute = '/splash';
+  static const String onboardingRoute = '/onboarding';
   static const String loginRoute = '/login';
   static const String signupRoute = '/signup';
   static const String homeRoute = '/home';
